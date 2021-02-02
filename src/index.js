fd6ca9e4f85bb9991b4a335b309c81f6dc7ad630
Artyom <Korvin1312b@yandex.by>
2021-02-02 10:54:36 +0300

    feat: add new function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = String(n).split("");
+    let arr2 = [];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] != "-") {
+            arr2.unshift(arr[i]);
+        }
+    }
+    return Number(arr2.join(""));
+};
